eb3ee67063050017255d3f46b5e48acafc291846
Sevugan <Meyyappan.Sevugan@cox.com>
2018-04-25 00:52:44 -0400

    .

--- string_data_structure.py
+++ string_data_structure.py
@@ -5,11 +5,15 @@ r = redis.Redis(host='ec2-54-235-235-122.compute-1.amazonaws.com',port='6379')
 r.set('first:key', '10')
 
 print r.get('first:key')
-
+print "\n"
 r.set('users:meyyappan', 'My first')
 
 print r.get('users:meyyappan')
-
+print "\n"
 r.append('users:meyyappan','car is mitsubishi')
 
-print "After append" + r.get('users:meyyappan')
+print "After append: " + r.get('users:meyyappan')
+print "\n"
+
+print "Substring: "  + r.getrange('users:meyyappan',10,20)
+
