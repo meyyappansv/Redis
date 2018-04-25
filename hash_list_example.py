4e58d6017ac0f772e57d828e7b886232f8f0c157
Sevugan <Meyyappan.Sevugan@cox.com>
2018-04-25 19:08:04 -0400

    .

--- hash_list_example.py
+++ hash_list_example.py
@@ -5,5 +5,5 @@ r = redis.Redis(host='ec2-54-235-235-122.compute-1.amazonaws.com',port='6379')
 
 user = {"name":"meyyappan","age":"38", "race":"asian"}
 r.hmset('user:info',user)
-print "The user info: " + r.hmget('user:info')
+print "The user info: " + r.hgetall('user:info')
 
