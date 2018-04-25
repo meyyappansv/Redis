c91ac1e6424ff1098eef153cbb521e631c50f88e
Sevugan <Meyyappan.Sevugan@cox.com>
2018-04-25 19:19:41 -0400

    .

--- hash_list_example.py
+++ hash_list_example.py
@@ -4,7 +4,9 @@ import os
 r = redis.Redis(host='ec2-54-235-235-122.compute-1.amazonaws.com',port='6379')
 
 user = {"name":"meyyappan","age":"38", "race":"asian"}
+user1 = {"name":"hari", "age":"38", "race":"asian"}
 r.hmset('user:info',user)
+r.hmset('user:info',user1)
 print "The user info: " + str(r.hgetall('user:info'))
 
-print "The name is % and the age is % "  % (r.hmget('user:info','name','age'))
+print r.hmget('user:info','name','age')
